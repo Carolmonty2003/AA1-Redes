7bc65e8cbf86bb187d624c0950826be116f7d55c
Carolina M.V. <Carolina.monteagudo@estudiant.enti.cat>
2026-03-05 13:34:27 +0100

    sfml + start code

--- Database_Test/Database_Test/Source.cpp
+++ Database_Test/Database_Test/Source.cpp
@@ -4,24 +4,21 @@
 #include "mysql_connection.h"
 #include <cppconn/driver.h>
 #include <cppconn/exception.h>
+#include <cppconn/prepared_statment.h>
+#include <cppconn/resultset.h>
 
 #define SERVER "127.0.0.1:3306"
 #define USERNAME "root"
 #define PASSWORD "enti"
+#define DATABASE "videogame"
 
-void main()
+void ConnectDatabase(sql::Diriver*& driver, sql::Connection*& con) 
 {
-    sql::Driver* driver;
-    sql::Connection* con;
-
     try
     {
         driver = get_driver_instance();
         con = driver->connect(SERVER, USERNAME, PASSWORD);
         std::cout << "Connection done." << std::endl;
-
-        con->close();
-        std::cout << "Connection close." << std::endl;
 
     }
     catch (sql::SQLException e)
@@ -30,6 +27,49 @@ void main()
         system("pause");
         exit(1);
     }
+}
 
-    delete con;
+void DisconnectDatabase(sql::Connection* con) 
+{
+    con->close();
+
+    if (con->isClosed()) 
+    {
+        std::cout << "Connection close." << std::endl;
+        delete con;
+    }
+}
+
+void GetAllUsers(sql::Connection* con) 
+{
+    try {
+        sql::Statement* stmt = con->createStatement();
+        sql::ResultSet* res = stmt->executeQuery("SELECT user FROM users");
+
+        std::cout << "Users in the database: " << std::endl;
+
+        while (res->next())
+        {
+        std::cout << res->getSring("users") << std::endl;
+        }
+    }
+    catch (sql::SQLException& e) {
+        std::cout << "Error while fetching users: " << e.what() << std::endl;
+    }
+
+    delete res;
+    delete stmt;
+}
+
+void main()
+{
+    sql::Driver* driver;
+    sql::Connection* con;
+    con->setSchema(DATABASE);
+
+    ConnectDatabase(driver, con);
+    GetAllUsers(con);
+    DisconnectDatabase(con);
+
+    system("pause");
 }
